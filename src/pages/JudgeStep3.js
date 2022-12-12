186438d06de93b495bc3aca5458dcd040159c998
S13740 <tigerus72@gmail.com>
2022-12-12 16:39:16 +0900

    modal 샘플

--- src/pages/JudgeStep3.js
+++ src/pages/JudgeStep3.js
@@ -15,18 +15,21 @@ function JudgeStep3() {
   //Modal pdf밸류 분할필요
   const values = [true];
   const [fullscreen, setFullscreen] = useState(true);
-  const [show, setShow] = useState(false);
 
-  function handleShow(breakpoint) {
-    setFullscreen(breakpoint);
-    setShow(true);
-  }
 
   const [visible, setVisible] = useState(false);
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [idxData, setIdxData] = useState(0);
+  
 
 
   return (
+    <>
     <Table className="TotalSection">
         <h2>정보제공동의 [필수] </h2>
           <b>대출 약정신청을 위해 아래 내용을 충분히 이해하신 후 동의하시기 바랍니다.</b>
@@ -41,16 +44,15 @@ function JudgeStep3() {
                   {data.contents}
                 </td>
                 <td>
-                    <Button key={idx} className="me-1 mb-1" onClick={() => handleShow(true)}>
+                    <Button key={idx} className="me-1 mb-1" 
+                            onClick={() => {
+                              setIdxData(idx);
+                              handleShow(true);
+                            }             
+                    }>
                       내용보기
                     </Button>
-                  <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-                    <Modal.Header closeButton>
-                      <Modal.Title></Modal.Title>
-                    </Modal.Header>
-                     <Modal.Body><iframe src={data.pdfvalue} height="100%" width="100%" title="ModalPdfViewer"/>
-                     </Modal.Body>
-                  </Modal> 
+                  
                 <Button className="me-2 mb-2" onClick={()=> {setVisible(!visible);}}>{visible ? "동의완료" : "동의"}</Button>
 
                 </td>
@@ -58,6 +60,7 @@ function JudgeStep3() {
                 {/**내용보기 Modal로 pdf파일 뷰어 출력 데이터??where*/}
 
               </tr>
+              
 
             )
           })
@@ -69,9 +72,23 @@ function JudgeStep3() {
             </tr>
       </tbody>
     </Table>
+    <ModalPdf show={show} handleClose={handleClose} handleShow={handleShow} data={jsonItemList} idx={idxData}></ModalPdf>
+  </>
   );
 }
 
+function ModalPdf(props) {
+  return (
+    <Modal show={props.show} onHide={props.handleClose} fullscreen={true}>
+    <Modal.Header closeButton>
+      <Modal.Title></Modal.Title>
+    </Modal.Header>
+     <Modal.Body>
+      <iframe src={props.data[props.idx].pdfvalue} height="100%" width="100%" title="ModalPdfViewer"/>
+     </Modal.Body>
+  </Modal>
+  );
+}
 
 
 export default JudgeStep3;
