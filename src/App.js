a4893a62bb85864cac8b11c81ddf2df0788f776c
Donghyup-Kim <donghyup56@naver.com>
2022-12-16 17:12:30 +0900

    22-12-16

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ import JudgeStep2 from './pages/JudgeStep2.js'
 import JudgeStep3 from './pages/JudgeStep3.js'
 import CsInfoAgree from './pages/CsInfoAgree.js'
 import CsInfoAgree2 from './pages/CsInfoAgree2.js';
-
+import DocCollectList from './pages/DocCollectList';
+import JudgeResult from './pages/JudgeResult';
+import JudgeDocResult from './pages/JudgeDocResult';
+import ProCheck from './pages/ProCheck';
+import PreJudge from './pages/PreJudge';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 
 
@@ -40,10 +44,26 @@ function App() {
       }} to="/judgestep3">심사준비 3단계</Link> ▷	▶
       <Link onClick={() => {
 
+      }} to="/judgeresult">적합성.적정성판단결과</Link> ▷ ▶
+      <Link onClick={() => {
+
       }} to="/csinfoagree">고객정보 위임 동의</Link> ▷ ▶
       <Link onClick={() => {
 
       }} to="/csinfoagree2">고객정보 위임 동의2</Link> ▷ ▶
+      <Link onClick={() => {
+
+      }} to="/doccollectlist">서류수집내역</Link> ▷ ▶
+      <Link onClick={() => {
+
+      }} to="/judgedocresult">사전심사 서류 수집현황</Link> ▷ ▶
+      <Link onClick={() => {
+
+      }} to="/procheck">대출 진행상태 조회</Link> ▷ ▶
+      <Link onClick={() => {
+
+}} to="/prejudge">사전심사 정보입력</Link> ▷ ▶
+
 
       <Link onClick={() => {
 
@@ -57,6 +77,11 @@ function App() {
         <Route path="/judgestep3" element={<JudgeStep3 />} />
         <Route path="/csinfoagree" element={<CsInfoAgree />} />
         <Route path="/csinfoagree2" element={<CsInfoAgree2 />} />
+        <Route path="/doccollectlist" element={<DocCollectList />} />
+        <Route path="/judgeresult" element={<JudgeResult />} />
+        <Route path="/judgedocresult" element={<JudgeDocResult />} />
+        <Route path="/procheck" element={<ProCheck />} />
+        <Route path="/prejudge" element={<PreJudge />} />
         <Route path="*" element={<div>404</div>} />
       </Routes>
     </div>
--- src/pages/CsInfoAgree2.js
+++ src/pages/CsInfoAgree2.js
@@ -1,3 +1,4 @@
+import CsInfoAgree2Css from '../css/CsInfoAgree2.css';
 import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from '../json/cstinfoAddData';
@@ -7,6 +8,7 @@ import { useEffect, useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Footer from './Footer.js';
+
 
 
 function CsInfoAgree2() {
@@ -20,33 +22,35 @@ function CsInfoAgree2() {
     return (
         <>
             <Header pageId={4} stepCd={3} />
-            <Table className=''>
+            <Table className='document3'>
                 <tr align='left'>
-                    <td><b>서류수집기관 : 국세청(홈텍스)</b></td>
-                </tr>
+                    <td><b><span2>서류수집기관 : 국세청(홈텍스)</span2></b></td>
+                </tr><br/>
                 <tr align='left'>
-                    <td>국세청(홈텍스)에서 서류를 수집하여 기업은행에 제출합니다.</td>
-                </tr>
-                <br/>
-                <br/>
+                    <td><div id="div_1">국세청(홈텍스)에서 서류를 수집하여 기업은행에 제출합니다.</div></td>
+                </tr><br/>
                 <tr align='left'>
-                    <td><b>국세청(홈텍스)에 등록되어있는 개인용 공동인증서</b>(금융 결제원 발급분만 가능, 범용인증서 불가)가 필요합니다.</td>
+                    <td><b><span2>국세청(홈텍스)에 등록되어있는 개인용 공동인증서</span2></b>
+                    <br/><br/><div id="div_1">(금융 결제원 발급분만 가능, 범용인증서 불가)가 필요합니다.</div></td><br/>
+                </tr><br/>
+                <div className='document4'>
+                <tr align='left'>
+                <ul>
+                    <tr>
+                    <li><td style={{ color: 'red' }}>개인용 공동인증서를 준비해주세요.</td></li>
+                    </tr><br/>
+                    <tr>
+                    <li><td> 먼저 국세청(홈텍스) 회원가입 및 개인공동인증서 등록 필요합니다.</td></li>
+                    </tr><br/>
+                    <tr>
+                    <li><td> PC에서 개인용 공동인증서를 국세청(홈텍스)에 등록하신 뒤 "기업은행 기업인터넷뱅킹" 접속&gt;인증센터&gt;공동인증서&gt;공동인증서복사&gt;스마트기기로 복사를 이용하시고 스마트폰에서는 "i-ONE기업뱅킹" 앱 실행&gt;인증센터&gt;공동인증서&gt;PC에서 가져오기 하셔야합니다.</td></li>
+                    </tr><br/>
+                    <tr>
+                    <li><td> 서류 수집분은 당일까지만 유효하며, 당일 사전심사신청을 완료하지 않은 경우 다음 영업일 자동 취소됩니다.</td></li>
+                    </tr><br/>
+                </ul>
                 </tr>
-
-                <tbody align='left'>
-                    <tr >
-                        <td style={{ color: 'red' }}>º 개인용 공동인증서를 준비해주세요.</td>
-                    </tr>
-                    <tr>
-                        <td>º 먼저 국세청(홈텍스) 회원가입 및 개인공동인증서 등록 필요합니다.</td>
-                    </tr>
-                    <tr>
-                        <td>º PC에서 개인용 공동인증서를 국세청(홈텍스)에 등록하신 뒤 "기업은행 기업인터넷뱅킹" 접속>인증센터>공동인증서>공동인증서복사>스마트기기로 복사를 이용하시고 스마트폰에서는 "i-ONE기업뱅킹" 앱 실행>인증센터>공동인증서>PC에서 가져오기 하셔야합니다.</td>
-                    </tr>
-                    <tr>
-                        <td>º 서류 수집분은 당일까지만 유효하며, 당일 사전심사신청을 완료하지 않은 경우 다음 영업일 자동 취소됩니다.</td>
-                    </tr>
-                </tbody>
+                </div>
 
             </Table>
             
@@ -55,5 +59,4 @@ function CsInfoAgree2() {
         
     )
 }
-
 export default CsInfoAgree2;
